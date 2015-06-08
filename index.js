702020562eac46d1ad2369c7eb667445572326df
mbonaci <mbonaci@gmail.com>
2015-06-08 09:52:36 +0200

    configuration done (configstore FTW)

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ var ronin = require('ronin');
 
 var program = ronin({
   path: __dirname,
-  desc: 'Logsene command-line interface',
-  options: {
-    'api-key': '',
-    'app-token': ''
-  }
+  desc: 'Logsene command-line interface'
 });
 
 program.run();
