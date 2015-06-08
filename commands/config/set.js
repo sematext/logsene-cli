8c6620f07ba7ce116f66d471a895a0cdf8003a33
mbonaci <mbonaci@gmail.com>
2015-06-08 09:50:12 +0200

    set command working - done

--- commands/config/set.js
+++ commands/config/set.js
@@ -1,35 +1,58 @@
 'use strict';
+/* jshint node:true */
+/* global module, process, console, require */
 
-var Command = require('ronin').Command,
-    argv    = require('minimist')(process.argv.slice(2)),
-    fs      = require('graceful-fs'),
-    os      = require('os'),
-    api     = require('../../lib/logsene-api');
+var Command     = require('ronin').Command,
+    argv        = require('minimist')(process.argv.slice(2)),
+    isEmpty     = require('../../lib/helpers').isEmpty,
+    out         = require('../../lib/helpers').out,
+    warnAndExit = require('../../lib/helpers').warnAndExit,
+    conf        = require('../../lib/config');
 
 
+var Set = Command.extend({ use: ['auth'],
+  desc: 'Set current user\'s configuration parameter(s)',
 
-var Set = Command.extend({ //use: ['auth'],
-  desc: 'Set client\'s configuration parameter',
+  run: function() {
+    out.trace('argv: ', argv); // TODO Cleanup
 
-  run: function () {
-    console.log(argv);  // TODO remove
-
-    if (Object.keys(argv).length < 3 || !argv.user || !argv.pass) {
-      console.error('Too few parameters\n');
-      console.log(this.help());
+    if (argv._.length !== 3) {
+      warnAndExit('Too few or too many parameters.\n\n', this);
     }
 
-    if (argv.password ) {
-
+    if (isEmpty(argv['api-key'])
+        && isEmpty(argv['app-key'])
+        && isEmpty(argv['app-name'])) {
+      warnAndExit('No known parameters specified.\n\n', this);
     }
+
+    // multiple option params allowed in a single command
+    if (isEmpty(argv['api-key'])) {
+      conf.setSync('api-key', argv['api-key']);
+      out.info('Success.')
+    }
+    if (isEmpty(argv['app-key'])) {
+      conf.setSync('app-key', argv['app-key']);
+      out.info('Success.')
+    }
+    if (isEmpty(argv['app-name'])) {
+      conf.setSync('app-name', argv['app-name']);
+      out.info('Success.')
+    }
+
+    process.exit(0); // bail out - that's it
   },
 
   // returns usage help
   help: function () {
     return 'Usage: logsene ' + this.name + ' [OPTIONS]\n' +
         '  where OPTIONS may be:\n' +
-        '    --user <username>\n' +
-        '    --pass <password>\n';
+        '    --api-key <apiKey>\n' +
+        '    --app-key <appKey>\n' +
+        '    --app-name <appName>\n'+
+        '    --trace\n' +
+        '  \n' +
+        '--------\n';
   }
 });
 
