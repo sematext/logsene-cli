03381f194f725678577c26e9a0d53ec5125601cb
mbonaci <mbonaci@gmail.com>
2015-06-11 10:01:00 +0200

    Transform stream to return only the _source subkey (like Logsene web app)

--- commands/search.js
+++ commands/search.js
@@ -2,88 +2,101 @@
 /* jshint node:true */
 /* global module, process, console, require */
 
-var Command     = require('ronin').Command,
-    spinner     = require('simple-spinner'),
-    argv        = require('minimist')(process.argv.slice(2)),
-    inspect     = require('util').inspect,
-    stream      = require('stream'),
-    JSONStream  = require('JSONStream'),
-    eventstream = require('event-stream'),
-    stringify   = require('../lib/helpers').safeJsonStringify,
-    out         = require('../lib/helpers').out,
-    conf        = require('../lib/config'),
-    api         = require('../lib/logsene-api');
+// not ideal place, but circular dependency otherwise
+require('../lib/bootstrap');  // throw away, just bootstrap
+
+var Command         = require('ronin').Command,
+    //spinner         = require('simple-spinner'),
+    inspect         = require('util').inspect,
+    JSONStream      = require('JSONStream'),
+    Transform       = require('stream').Transform,
+    eos             = require('end-of-stream'),
+    argv            = require('../lib/helpers').argv,
+    out             = require('../lib/helpers').out,
+    isDef           = require('../lib/helpers').isDef,
+    isStrOrBoolTrue = require('../lib/helpers').isStrOrBoolTrue,
+    conf            = require('../lib/config'),
+    api             = require('../lib/logsene-api');
 
 //var $this = this;
-spinner.change_sequence(["◓", "◑", "◒", "◐"]);
+//spinner.change_sequence(["◓", "◑", "◒", "◐"]);
 
 
 var Search = Command.extend({ use: ['auth'],
   desc: 'Search Logsene logs',
 
-  run: function () {
+  run: function _run() {
     inspect(argv); // TODO clean
 
-    // Object.keys(argv).length < 2
     if (argv._.length < 2) {
-      console.warn(out.warn('Too few parameters!\n\n'));
-      console.log(this.help());
+      out.warn('Too few parameters!\n\n');
+      out.info(this.help());
       process.exit(1); // bail out
     } else if (argv._.length === 2) {
       // if no -q use first command after search as q
-      // so we can do 'logsene search <query>'
+      // so 'logsene search <query>' works as expected
       argv.q = argv._[1];
     }
 
-    //spinner.start();  // TODO replace with isaacs spinner, which doesn't leave anything behind
+    //spinner.start();
     api.search({
-      q:      argv.q,
-      appKey: conf.getSync('appKey'),
-      apiKey: conf.getSync('apiKey'),
-      size:   argv.size || 200,
-      offset: argv.offset || 0
-    }, function(err, esReadable) {
+      q:        argv.q,
+      apiKey:   conf.getSync('apiKey'),
+      appKey:   conf.getSync('appKey'),
+      size:     argv.size || conf.getSync('defaultSize') || conf.maxHits,
+      offset:   argv.offset || 0,
+      logLevel: argv.trace || isStrOrBoolTrue(conf.getSync('trace')) ? 'trace' : 'error'
+  }, function(err, esReadableHits) {
       //spinner.stop();
       if(err) {
-        console.error(out.error('Search error: ' + err.message));
+        out.error('Search error: ' + err.message);
         process.exit(1); // bail out
       }
 
-      // TODO return final counter
-      //console.log(out.info(hits.hits.length + ' results:'));
+      var extractor = new Transform({objectMode: true}),
+          hitCnt = 0;
 
-      // TODO pass back readable stream and handle the rest from here
-      /*esReadable
-          .pipe(JSONStream.parse('_source.*'))
-          .pipe(eventstream.mapSync(function(data) {
-            //out.info(data);
-            return data._source || data;
-          })).pipe(process.stdout);*/
+      extractor._transform = function _extractorTransform(data, encoding, next) {
+        this.push(isDef(data['_source']) ? data['_source'] : data);
+        hitCnt++;
+        next();
+      };
 
+      eos(esReadableHits, function(err) {
+        if (err) {
+          out.error('ES stream had an error or closed early.');
+          process.exit(1);
+        }
+        out.info('\nReturned hits: ' + hitCnt + (hitCnt === 200 ? ' (max)' : ''));
+        process.exit(0);
+      });
 
-      //console.log(stringify(hits));
-      //esReadable.on('close', function() {process.exit(0);})
+      // pipe em up
+      esReadableHits
+          .pipe(extractor)
+          .pipe(JSONStream.stringify(false))
+          .pipe(process.stdout);
+
     });
-
   },
 
 
-  help: function () {
+  help: function _help() {
     return 'Usage: logsene ' + this.name + ' query [OPTIONS]\n' +
     '  where OPTIONS may be:\n' +
-    '    --q <search-query>       (Optional, but then query must come immediately after keyword ' + this.name + ')\n' +
-    '    --size <return-size>     (Number of matches to return. Defaults to 20)\n' +
-    '    --offset <return-offset> (Number of matches to skip. Defaults to 0)\n' +
+    '    --q <search-query>    (Query string)\n' +
+    '    --s <return-size>     (Number of matches to return. Defaults to 200)\n' +
+    '    --o <return-offset>   (Number of matches to skip. Defaults to 0)\n' +
     '\n' +
     '  Examples:\n' +
     '  logsene search --q ERROR --s 50\n' +
-    '  returns at most 50 matches of term ERROR\n' +
+    '      returns at most 50 matches of term ERROR\n' +
     '\n' +
     '  logsene search ERROR --s 50\n' +
-    '  equivalent to first example\n' +
+    '      equivalent to the previous example\n' +
     '\n' +
-    '  logsene search ERROR --s 50 --offset 20\n' +
-    '  returns hits from 21th to 71th (useful for paging)\n' +
+    '  logsene search ERROR --s 50 --o 20\n' +
+    '      returns hits from 21th to 71th\n' +
     '  \n' +
     '--------\n';
   }
