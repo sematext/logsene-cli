12f2f01fb4fc6f6a2a87931a001ebf5fc65ceec2
mbonaci <mbonaci@gmail.com>
2015-06-15 10:44:38 +0200

    Make tsv default and allow json when explicitly asked; Update help

--- commands/search.js
+++ commands/search.js
@@ -6,9 +6,10 @@
 require('../lib/bootstrap');  // throw away, just bootstrap
 
 var Command         = require('ronin').Command,
-    //spinner         = require('simple-spinner'),
     inspect         = require('util').inspect,
     JSONStream      = require('JSONStream'),
+    values          = require('lodash.values'),
+    forEach         = require('lodash.foreach'),
     Transform       = require('stream').Transform,
     eos             = require('end-of-stream'),
     argv            = require('../lib/helpers').argv,
@@ -18,9 +19,6 @@ var Command         = require('ronin').Command,
     conf            = require('../lib/config'),
     api             = require('../lib/logsene-api');
 
-//var $this = this;
-//spinner.change_sequence(["◓", "◑", "◒", "◐"]);
-
 
 var Search = Command.extend({ use: ['session', 'auth'],
   desc: 'Search Logsene logs',
@@ -29,7 +27,7 @@ var Search = Command.extend({ use: ['session', 'auth'],
     inspect(argv); // TODO clean
 
     if (argv._.length < 2) {
-      out.warn('Too few parameters!\n\n');
+      out.warn('Too few parameters!\n');
       out.info(this.help());
       process.exit(1); // bail out
     } else if (argv._.length === 2) {
@@ -38,7 +36,7 @@ var Search = Command.extend({ use: ['session', 'auth'],
       argv.q = argv._[1];
     }
 
-    //spinner.start();
+
     api.search({
       q:        argv.q,
       apiKey:   conf.getSync('apiKey'),
@@ -47,35 +45,59 @@ var Search = Command.extend({ use: ['session', 'auth'],
       offset:   argv.o || 0,
       logLevel: argv.trace || isStrOrBoolTrue(conf.getSync('trace')) ? 'trace' : 'error'
   }, function(err, esReadableHits) {
-      //spinner.stop();
       if(err) {
         out.error('Search error: ' + err.message);
         process.exit(1); // bail out
       }
 
-      var extractor = new Transform({objectMode: true}),
+      var jsonExtractor = new Transform({objectMode: true}),
+          tsvExtractor = new Transform({objectMode: true}),
           hitCnt = 0;
 
-      extractor._transform = function _extractorTransform(data, encoding, next) {
-        this.push(isDef(data['_source']) ? data['_source'] : data);
+      jsonExtractor._transform = function _jsonTransform(data, encoding, next) {
+        var source = isDef(data['_source']) ? data['_source'] : data;
+
+        this.push(source);
         hitCnt++;
         next();
       };
 
-      eos(esReadableHits, function(err) {
+
+      tsvExtractor._transform = function _tsvTransform(data, encoding, next) {
+        var source = isDef(data['_source']) ? data['_source'] : data;
+        var output = '';
+
+        forEach(values(source), function _forEachValue(v) {
+          output += v + '\t';
+        });
+
+        this.push(output + '\n');
+        hitCnt++;
+        next();
+      };
+
+      eos(esReadableHits, function _esReadableHits(err) {
         if (err) {
           out.error('ES stream had an error or closed early.');
           process.exit(1);
         }
-        out.info('\nReturned hits: ' + hitCnt + (hitCnt === 200 ? ' (max)' : ''));
+        // remove so that client gets pure tsv or json
+        //out.info('\nReturned hits: ' + hitCnt + (hitCnt === 200 ? ' (max)' : ''));
         process.exit(0);
       });
 
-      // pipe em up
-      esReadableHits
-          .pipe(extractor)
-          .pipe(JSONStream.stringify(false))
-          .pipe(process.stdout);
+
+      if (argv.json) {
+        esReadableHits
+            .pipe(jsonExtractor)
+            .pipe(JSONStream.stringify(false))
+            .pipe(process.stdout);
+
+      } else {
+        esReadableHits
+            .pipe(tsvExtractor)
+            .pipe(process.stdout);
+      }
 
     });
   },
@@ -84,21 +106,23 @@ var Search = Command.extend({ use: ['session', 'auth'],
   help: function _help() {
     return 'Usage: logsene ' + this.name + ' query [OPTIONS]\n' +
     '  where OPTIONS may be:\n' +
-    '    --q <search-query>    (Query string)\n' +
-    '    --s <return-size>     (Number of matches to return. Defaults to 200)\n' +
-    '    --o <return-offset>   (Number of matches to skip. Defaults to 0)\n' +
+    '    --q <query>    (Query string)\n' +
+    '    --s <size>     (Number of matches to return. Defaults to ' + conf.maxHits + ')\n' +
+    '    --o <offset>   (Number of matches to skip. Defaults to 0)\n' +
+    '    --json         (Returns JSON instead of TSV)\n' +
+    '    --trace        (Turns on one-time tracing. Use only for troubleshooting, it messes up output)\n' +
     '\n' +
-    '  Examples:\n' +
-    '  logsene search --q ERROR --s 50\n' +
+    'Examples:\n' +
+    '  logsene ' + this.name + ' --q ERROR --s 50\n' +
     '      returns at most 50 highest ranked matches of term ERROR\n' +
     '\n' +
-    '  logsene search ERROR --s 50\n' +
+    '  logsene ' + this.name + ' ERROR --s 50\n' +
     '      equivalent to the previous example\n' +
     '\n' +
-    '  logsene search ERROR --s 50 --o 20\n' +
+    '  logsene ' + this.name + ' ERROR --s 50 --o 20\n' +
     '      returns hits from 21th to 71th\n' +
-    '  \n' +
-    '--------\n';
+    '\n' +
+    '--------';
   }
 });
 
